6bf11bb8246a941855b7afd1b4467e0628cc9b85
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2023-02-10 12:36:00 +0000

    primo commit

--- src/app/mock-cars.ts
+++ src/app/mock-cars.ts
@@ -1,9 +1,9 @@
 import { Car } from './car';
 
 export const CARS: Car[] = [
-  { id: 11, name: 'Putno', costo:10, consumo:25 },
-  { id: 12, name: 'Bravo', costo:20, consumo:20 }, 
-  { id: 13, name: 'Tipo', costo:15, consumo:10 }, 
-  { id: 14, name: '124_spider' , costo:100, consumo:90 },
-  { id: 15, name: '500', costo:1000, consumo:33 }
+  { id: 11, name: 'Carlo', costo:10, consumo:25 },
+  { id: 12, name: 'Pino', costo:20, consumo:20 }, 
+  { id: 13, name: 'Sondel', costo:15, consumo:10 }, 
+  { id: 14, name: 'Godrolo' , costo:100, consumo:90 },
+  { id: 15, name: '420', costo:1000, consumo:33 }
 ];
